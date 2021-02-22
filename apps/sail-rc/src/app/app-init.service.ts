04aa41fde150882504ebb79c895d934bb708f118
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2021-02-22 07:28:01 +0100

    Make ESLint happy.

--- apps/sail-rc/src/app/app-init.service.ts
+++ apps/sail-rc/src/app/app-init.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AppInitService {
 
-//  constructor( private httpClient: HttpClient ) {}
+ init() {
+   const apiKey = environment.googleCloudPlatform.apiKey;
+   const mapsAPIUrl =`/google/maps/api/js?key=${apiKey}`;
 
-//  init() {
-//    const apiKey = environment.googleCloudPlatform.apiKey;
-//    const mapsAPIUrl =`/google/maps/api/js?key=${apiKey}`;
-
+   console.log( mapsAPIUrl );
     // return this.httpClient.jsonp( mapsAPIUrl, 'callback' ).toPromise().then( async x => {
     //   await new Promise( resolve => setTimeout( resolve, 5000 ));
     //   console.log( 'Google Maps API loaded.');
     // });
-//  }
+  }
 }
