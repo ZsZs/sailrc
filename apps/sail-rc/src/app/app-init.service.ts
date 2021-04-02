7be86bfa7f854897ccef0e44090e3e7a383b5fd5
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2021-04-02 19:37:58 +0200

    Feature/select race (#8)
    * Debug setEnv.ts
    
    * Add API keys to travis.
    
    * increase security.
    
    * uncomment init()

--- apps/sail-rc/src/app/app-init.service.ts
+++ apps/sail-rc/src/app/app-init.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class AppInitService {
 
-//  init() {
+  init() {
 //  const apiKey = environment.googleCloudPlatform.apiKey;
 //  const mapsAPIUrl =`/google/maps/api/js?key=${apiKey}`;
-//  }
+  }
 }
