c36454cb5665a0b452705c97e18b3933f19701a8
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2021-04-15 18:07:38 +0200

    Write unit test for AppInitService.

--- apps/sail-rc/src/app/app-init.service.ts
+++ apps/sail-rc/src/app/app-init.service.ts
@@ -7,13 +7,12 @@ export class AppInitService {
 
   constructor( private httpClient: HttpClient ) {}
 
-  init() {
+  init(): Promise<boolean | void> {
    const apiKey = environment.googleCloudPlatform.apiKey;
    const mapsAPIUrl =`https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
 
-    return this.httpClient.jsonp( mapsAPIUrl, 'callback' ).toPromise().then( async x => {
+    return this.httpClient.jsonp( mapsAPIUrl, 'callback' ).toPromise().then( async () => {
       await new Promise( resolve => setTimeout( resolve, 5000 ));
-      console.log( 'Google Maps API loaded.');
     });
   }
 }
